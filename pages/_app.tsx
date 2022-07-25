4be20c21f4045f4bf965fa5fe8610fe720c826bc
Dowland Aiello <dowlandaiello@gmail.com>
2022-07-25 00:12:58 -0400

    Create an eslint config to appease vercel.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { Component } from "react";
 
-function MyApp({ Component, pageProps }) {
+export const MyApp = ({
+	Component,
+	pageProps,
+}: {
+	Component: Component;
+	pageProps: unknown[];
+}) => {
 	return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
