309b4736c554a35f2c859aa38b7c59a597333930
Dowland Aiello <dowlandaiello@gmail.com>
2022-07-25 18:15:13 -0400

    Add a local-dirs file that registers the org project.

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((nil . ((create-lockfiles . nil)))
+ (org-mode . ((eval . (setq org-publish-project-alist (cons
+													   (let ((based (file-name-as-directory (projectile-project-root))))
+														 (list "roadmap"
+															   :base-directory (concat based "content")
+															   :recursive t
+															   :publishing-directory (concat based "public")
+															   :index-filename "Status.org"
+															   :index-title "Vision Development Roadmap"))
+													   (cond ((boundp 'org-publish-project-alist) org-publish-project-alist)
+															 (t '()))))))))
